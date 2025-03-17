9a6d8b82e7de339246b4f2fe1b8cb31cec4865b1
Jackbear8868 <leo1314520528868@gmail.com>
2025-03-17 15:05:55 +0800

    hw1-2:A lot of Errors :<

--- hw1_material/part2/JBF.py
+++ hw1_material/part2/JBF.py
@@ -2,7 +2,6 @@
 import numpy as np
 import cv2
 
-
 class Joint_bilateral_filter(object):
     def __init__(self, sigma_s, sigma_r):
         self.sigma_r = sigma_r
@@ -16,5 +15,37 @@ class Joint_bilateral_filter(object):
         padded_guidance = cv2.copyMakeBorder(guidance, self.pad_w, self.pad_w, self.pad_w, self.pad_w, BORDER_TYPE).astype(np.int32)
 
         ### TODO ###
-        
+        h, w = img.shape[:2]
+        output = np.zeros_like(img, dtype=np.float32)
+
+        # Precompute spatial kernel (fixed for the entire image)
+        offset = np.arange(-self.pad_w, self.pad_w + 1, dtype=np.float32)
+        x, y = np.meshgrid(offset, offset)
+        G_s = np.exp(-(x**2 + y**2) / (2 * self.sigma_s**2))
+
+        for i in range(h):
+            for j in range(w):
+                i_p, j_p = i + self.pad_w, j + self.pad_w # Adjusted index in padded image
+
+                # Extract local window
+                img_window = padded_img[i_p - self.pad_w:i_p + self.pad_w + 1, j_p - self.pad_w:j_p + self.pad_w + 1]
+                guidance_window = padded_guidance[i_p - self.pad_w:i_p + self.pad_w + 1, j_p - self.pad_w:j_p + self.pad_w + 1]
+                
+                # Compute range kernel G_r using guidance image
+                if guidance.ndim == 2:  # Grayscale image
+                    G_r = np.exp(-((guidance_window - padded_guidance[i_p, j_p])**2) / (2 * self.sigma_r**2))
+                else:  # RGB image
+                    diff_r = (guidance_window[:, :, 0] - padded_guidance[i_p, j_p, 0])**2
+                    diff_g = (guidance_window[:, :, 1] - padded_guidance[i_p, j_p, 1])**2
+                    diff_b = (guidance_window[:, :, 2] - padded_guidance[i_p, j_p, 2])**2
+                    G_r = np.exp(-(diff_r + diff_g + diff_b) / (2 * self.sigma_r**2))
+                
+                # Compute final joint bilateral weights
+                weights = G_s * G_r
+
+                if img.ndim == 2:  # Grayscale image
+                    output[i, j] = np.sum(weights * img_window) / np.sum(weights)
+                else:  # RGB image (process each channel separately)
+                    output[i, j] = np.sum(weights[..., np.newaxis] * img_window, axis=(0, 1)) / np.sum(weights)
+
         return np.clip(output, 0, 255).astype(np.uint8)
--- hw1_material/part2/main.py
+++ hw1_material/part2/main.py
@@ -4,6 +4,18 @@ import argparse
 import os
 from JBF import Joint_bilateral_filter
 
+def calculate_Y(self, image, wr, wg, wb):
+    # Extract R, G, B channels
+    R = image[:, :, 0].astype(np.float32)
+    G = image[:, :, 1].astype(np.float32)
+    B = image[:, :, 2].astype(np.float32)
+
+    # Compute Y channel
+    Y = wr * R + wg * G + wb * B
+
+    return Y.astype(np.uint8)  # Convert back to uint8 for image processing
+
+
 
 def main():
     parser = argparse.ArgumentParser(description='main function of joint bilateral filter')
@@ -16,7 +28,8 @@ def main():
     img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
     ### TODO ###
-
+    wr, wg, wb = 0.5, 0.3, 0.2
+    Y = calculate_Y()
 
 if __name__ == '__main__':
     main()
