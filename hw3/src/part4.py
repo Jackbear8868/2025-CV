a5c4e6c1dea8bffe08c2d0d4e5cd71b4fb0592e3
Jackbear8868 <leo1314520528868@gmail.com>
2025-05-08 09:01:25 +0800

    hw3-part4

--- hw3/src/part4.py
+++ hw3/src/part4.py
@@ -20,21 +20,68 @@ def panorama(imgs):
     dst[:imgs[0].shape[0], :imgs[0].shape[1]] = imgs[0]
     last_best_H = np.eye(3)
     out = None
-
+    # Initiate ORB detector
+    orb = cv2.ORB_create()
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    threshold = 3
+    max_iter = 5000
     # for all images to be stitched:
     for idx in tqdm(range(len(imgs)-1)):
         im1 = imgs[idx]
         im2 = imgs[idx + 1]
 
         # TODO: 1.feature detection & matching
-
+        # find the keypoints and descriptors with ORB
+        kp1, des1 = orb.detectAndCompute(im1,None)
+        kp2, des2 = orb.detectAndCompute(im2,None)
+        # Match descriptors.
+        matches = bf.match(des1,des2)
+        
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key = lambda x:x.distance)
+    
         # TODO: 2. apply RANSAC to choose best H
+        max_inliers = []
+        best_H = None
+
+        for i in range(max_iter):
+            subset = random.sample(matches, 4)
+            u = np.array([kp2[m.trainIdx].pt for m in subset])
+            v = np.array([kp1[m.queryIdx].pt for m in subset])
+
+            H = solve_homography(u, v)
+            if H is None:
+                continue
+
+            inliers = []
+            for m in matches:
+                pt1 = np.array([*kp1[m.queryIdx].pt, 1.0])
+                pt2 = np.array([*kp2[m.trainIdx].pt, 1.0])
+
+                projected = H @ pt2
+                projected /= projected[2]
+
+                error = np.linalg.norm(projected[:2] - pt1[:2])
+                if error < threshold:
+                    inliers.append(m)
+
+            if len(inliers) > len(max_inliers):
+                max_inliers = inliers
+                best_H = H
+
+        u_best = np.array([kp2[m.trainIdx].pt for m in max_inliers])
+        v_best = np.array([kp1[m.queryIdx].pt for m in max_inliers])
+        best_H = solve_homography(u_best, v_best)
 
         # TODO: 3. chain the homographies
+        last_best_H = last_best_H @ best_H
 
         # TODO: 4. apply warping
-
-    return out 
+        h, w = dst.shape[:2]
+        dst = warping(im2, dst, last_best_H, 0, h, 0, w, direction='b')
+        out = dst
+    return out
 
 if __name__ == "__main__":
     # ================== Part 4: Panorama ========================
